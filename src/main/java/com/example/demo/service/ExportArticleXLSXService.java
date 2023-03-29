71695e2e917ee0110759bdccff68406a0e520b6c
Alexandre PETRILLO <alexandre-externe.petrillo@enedis.fr>
2023-03-29 16:22:53 +0200

    utilisation des entity dans le service pour simplifier le traitement

--- src/main/java/com/example/demo/service/ExportArticleXLSXService.java
+++ src/main/java/com/example/demo/service/ExportArticleXLSXService.java
@@ -5,8 +5,11 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.List;
 
+import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -14,16 +17,23 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.dto.ArticleDto;
+import com.example.demo.entity.Article;
+import com.example.demo.repository.ArticleRepository;
 
 @Service
 public class ExportArticleXLSXService {
 
     @Autowired
-    private ArticleService articleService;
+    private ArticleRepository articleRepository;
 
     public void export(OutputStream outputStream) throws IOException {
         Workbook workbook = new XSSFWorkbook();
         Sheet sheet = workbook.createSheet("Article");
+
+        CellStyle cellStyle = workbook.createCellStyle();
+        cellStyle.setBorderBottom(BorderStyle.DASHED);
+        cellStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.getIndex());
+        cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 
         Row headerRow = sheet.createRow(0);
         Cell cellHeaderLibelle = headerRow.createCell(0);
@@ -33,13 +43,13 @@ public class ExportArticleXLSXService {
         cellHeaderPrix.setCellValue("Prix");
 
         int iRow = 1;
-        for (ArticleDto articleDto : articleService.findAll()) {
+        for (Article article : articleRepository.findAll()) {
             Row row = sheet.createRow(iRow++);
             Cell cellLibelle = row.createCell(0);
             Cell cellPrix = row.createCell(1);
 
-            cellLibelle.setCellValue(articleDto.getLibelle());
-            cellPrix.setCellValue(articleDto.getPrix());
+            cellLibelle.setCellValue(article.getLibelle());
+            cellPrix.setCellValue(article.getPrix());
         }
 
         workbook.write(outputStream);
--- src/main/java/com/example/demo/service/ExportFacturesXLSXService.java
+++ src/main/java/com/example/demo/service/ExportFacturesXLSXService.java
@@ -3,22 +3,20 @@ package com.example.demo.service;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.example.demo.dto.ArticleDto;
+import com.example.demo.repository.ArticleRepository;
+import com.example.demo.repository.ClientRepository;
 
 @Service
 public class ExportFacturesXLSXService {
 
     @Autowired
-    private ArticleService articleService;
+    private ArticleRepository articleRepository;
     @Autowired
-    private ClientService clientService;
+    private ClientRepository clientRepository;
 
     public void export(OutputStream outputStream) throws IOException {
         Workbook workbook = new XSSFWorkbook();
